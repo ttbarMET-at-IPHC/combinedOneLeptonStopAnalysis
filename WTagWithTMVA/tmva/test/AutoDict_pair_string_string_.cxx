c0a4af9b158b0c4e38cd762755ef9adfef981cef
Alexandre Aubin <alexandre.aubin@cern.ch>
2013-09-03 17:11:04 +0200

    Renamed WTag TMVA test folders

--- WTagWithTMVA/tmva/test/AutoDict_pair_string_string_.cxx
+++
@@ -1,9 +0,0 @@
-#include "vector"
-#include "string"
-#include "string"
-#ifdef __CINT__ 
-#pragma link C++ nestedclasses;
-#pragma link C++ nestedtypedefs;
-#pragma link C++ class pair<string,string>+;
-#pragma link C++ class pair<string,string>::*+;
-#endif
