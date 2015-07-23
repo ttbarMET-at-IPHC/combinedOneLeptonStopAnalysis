6bf95f3b9f5294bcc0a5b2296c1c3358c3df0ed9
Eric Chabert <Eric.Chabert@cern.ch>
2015-07-23 07:36:18 +0200

    new file

---
+++ backgroundEstimation/mcStudies/.vectorDictionnary.C
@@ -0,0 +1,10 @@
+#include "TLorentzVector.h"
+#include <vector>
+
+#ifdef __MAKECINT__
+#pragma link C++ class vector<TLorentzVector >+;
+#pragma link C++ class vector<float >+;
+#pragma link C++ class vector<short int >+;
+#pragma link C++ class vector<vector< int > >+;
+#pragma linc C++ class vector<TLorentzVector>+;
+#endif
